74ad00086719bc6f78acfe5b2bc037a4b87f65f7
clemensolana <clementesolana@outlook.com>
2024-11-04 16:05:38 +0100

    feat: agrego funcionalidad de menu hamburguesa y script js a index.html

---
+++ public/js/burgerMenu/burgerMenu.js
@@ -0,0 +1,26 @@
+const burgerMenuIcon = document.querySelector('.burgerMenu');
+const navMenu = document.querySelector('.navContainer ul');
+const main = document.querySelector('main');
+const footer = document.querySelector('footer');
+
+burgerMenuIcon.addEventListener('click', () => {
+  if (window.innerWidth <= 768) {
+    if (navMenu.style.display === 'none') {
+      navMenu.style.display = 'block';
+      main.style.display = 'none';
+      footer.style.display = 'none';
+    } else {
+      navMenu.style.display = 'none';
+      main.style.display = '';
+      footer.style.display = '';
+    }
+  }
+});
+
+window.addEventListener('resize', () => {
+  if (window.innerWidth >= 768) {
+    navMenu.style.display = 'flex';
+    main.style.display = 'flex';
+    footer.style.display = 'flex';
+  }
+});
