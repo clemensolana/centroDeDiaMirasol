759ccb4f07d4556b170781121f46dfbaa46e926b
clemensolana <clementesolana@outlook.com>
2024-11-21 20:33:23 +0100

    fix: soluciono error que hacía que tenga que tocar dos veces la primera vez que se ingresaba a una vista el burgerMenu para que funcione.

--- public/js/burgerMenu/burgerMenu.js
+++ public/js/burgerMenu/burgerMenu.js
@@ -1,26 +1,28 @@
-const burgerMenuIcon = document.querySelector('.burgerMenu');
-const navMenu = document.querySelector('.navContainer ul');
-const main = document.querySelector('main');
-const footer = document.querySelector('footer');
+const burgerMenuIcon = document.querySelector(".burgerMenu");
+const navMenu = document.querySelector(".navContainer ul");
+const main = document.querySelector("main");
+const footer = document.querySelector("footer");
 
-burgerMenuIcon.addEventListener('click', () => {
+navMenu.style.display = "none";
+
+burgerMenuIcon.addEventListener("click", () => {
   if (window.innerWidth <= 768) {
-    if (navMenu.style.display === 'none') {
-      navMenu.style.display = 'block';
-      main.style.display = 'none';
-      footer.style.display = 'none';
+    if (navMenu.style.display === "none") {
+      navMenu.style.display = "block";
+      main.style.display = "none";
+      footer.style.display = "none";
     } else {
-      navMenu.style.display = 'none';
-      main.style.display = '';
-      footer.style.display = '';
+      navMenu.style.display = "none";
+      main.style.display = "";
+      footer.style.display = "";
     }
   }
 });
 
-window.addEventListener('resize', () => {
+window.addEventListener("resize", () => {
   if (window.innerWidth >= 768) {
-    navMenu.style.display = 'flex';
-    main.style.display = 'flex';
-    footer.style.display = 'flex';
+    navMenu.style.display = "flex";
+    main.style.display = "flex";
+    footer.style.display = "flex";
   }
 });
