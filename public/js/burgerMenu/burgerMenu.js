1b4875744beb53dfccc88426792b6dc6cc322cb4
clemensolana <clementesolana@outlook.com>
2024-11-22 18:25:44 +0100

    fix: soluciono problema que al redimensionar la pantalla siendo >= 768 o al desplazarte dentro del menú hamburguesa de haga display "none" y ya no se pueda visualizar.

--- public/js/burgerMenu/burgerMenu.js
+++ public/js/burgerMenu/burgerMenu.js
@@ -4,18 +4,20 @@ const main = document.querySelector("main");
 const footer = document.querySelector("footer");
 
 function updateMenuDisplay() {
-  if (window.innerWidth >= 767) {
+  if (window.innerWidth >= 768) {
     navMenu.style.display = "";
     main.style.display = "";
     footer.style.display = "";
   } else {
-    navMenu.style.display = "none";
+    if (navMenu.style.display !== "block") {
+      navMenu.style.display = "none";
+    }
   }
 }
 updateMenuDisplay();
 
 burgerMenuIcon.addEventListener("click", () => {
-  if (window.innerWidth <= 767) {
+  if (window.innerWidth < 768) {
     if (navMenu.style.display === "none") {
       navMenu.style.display = "block";
       main.style.display = "none";
