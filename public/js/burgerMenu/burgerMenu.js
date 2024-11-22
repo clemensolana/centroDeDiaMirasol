b8cd5285275705596fe60b05ea0319b12cc17a7f
clemensolana <clementesolana@outlook.com>
2024-11-22 19:51:30 +0100

    feat: implemento transiciones para el botón de cierre y para el menú hamburguesa

--- public/js/burgerMenu/burgerMenu.js
+++ public/js/burgerMenu/burgerMenu.js
@@ -1,4 +1,5 @@
 const burgerMenuIcon = document.querySelector(".burgerMenu");
+const xMenuButton = document.querySelector(".xButtonMenu");
 const navMenu = document.querySelector(".navContainer ul");
 const main = document.querySelector("main");
 const footer = document.querySelector("footer");
@@ -9,9 +10,18 @@ function updateMenuDisplay() {
     navMenu.style.display = "";
     main.style.display = "";
     footer.style.display = "";
+    burgerMenuIcon.style.display = "none";
+    xMenuButton.style.display = "none";
+    burgerMenuIcon.style.opacity = "1";
   } else {
     if (!navMenu.classList.contains("show")) {
       navMenu.style.display = "none";
+      burgerMenuIcon.style.display = "block";
+      burgerMenuIcon.style.opacity = "1";
+      xMenuButton.style.display = "none";
+    } else {
+      burgerMenuIcon.style.display = "none";
+      xMenuButton.style.display = "block";
     }
   }
 }
@@ -28,7 +38,19 @@ burgerMenuIcon.addEventListener("click", () => {
       navMenu.style.display = "block";
       main.style.display = "none";
       footer.style.display = "none";
-    } else if (navMenu.classList.contains("show")) {
+      burgerMenuIcon.style.opacity = "0";
+      setTimeout(() => {
+        burgerMenuIcon.style.display = "none";
+        xMenuButton.style.display = "block";
+        xMenuButton.style.opacity = "1";
+      }, 300);
+    }
+  }
+});
+
+xMenuButton.addEventListener("click", () => {
+  if (window.innerWidth < 768) {
+    if (navMenu.classList.contains("show")) {
       navMenu.classList.remove("show");
       navMenu.classList.add("hide");
       setTimeout(() => {
@@ -38,6 +60,12 @@ burgerMenuIcon.addEventListener("click", () => {
         main.classList.add("fade-in");
         setTimeout(() => {
           main.classList.remove("fade-in");
+        }, 300);
+        xMenuButton.style.opacity = "0";
+        setTimeout(() => {
+          xMenuButton.style.display = "none";
+          burgerMenuIcon.style.display = "block";
+          burgerMenuIcon.style.opacity = "1";
         }, 300);
       }, 300);
     }
