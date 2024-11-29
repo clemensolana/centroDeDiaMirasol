805fd51a61193fb88e898d9cba304e3a6c7dd83c
clemensolana <clementesolana@outlook.com>
2024-11-29 15:37:49 +0100

    feat: agrego rutas a intranet /intranet

--- src/routes/mainRoutes.js
+++ src/routes/mainRoutes.js
@@ -1,23 +1,27 @@
 // Require's
-const express = require('express');
+const express = require("express");
 const mainRouter = express.Router();
 
 // Controller require
-const mainController = require('../controllers/mainController');
+const mainController = require("../controllers/mainController");
 
 // Main routes
-mainRouter.get('/', mainController.index);
-mainRouter.get('/services', mainController.services);
-mainRouter.get('/installations', mainController.installations);
-mainRouter.get('/aboutUs', mainController.aboutUs);
-mainRouter.get('/contactUs', mainController.contactForm);
-mainRouter.get('/news', mainController.news);
-mainRouter.get('/aNew', mainController.aNew);
-mainRouter.get('/termAndConditions', mainController.termsAndConditions);
-mainRouter.get('/dataProtection', mainController.dataProtection);
+mainRouter.get("/", mainController.index);
+mainRouter.get("/services", mainController.services);
+mainRouter.get("/installations", mainController.installations);
+mainRouter.get("/aboutUs", mainController.aboutUs);
+mainRouter.get("/contactUs", mainController.contactForm);
+mainRouter.get("/news", mainController.news);
+mainRouter.get("/aNew", mainController.aNew);
+mainRouter.get("/termAndConditions", mainController.termsAndConditions);
+mainRouter.get("/dataProtection", mainController.dataProtection);
 
 // Email routes
-const emailRoutes = require('./emailRoutes.js');
-mainRouter.use('/email', emailRoutes);
+const emailRoutes = require("./emailRoutes.js");
+mainRouter.use("/email", emailRoutes);
+
+// Intranet routes
+const intranetRoutes = require("./intranetRoutes/mainIntranetRoutes.js");
+mainRouter.use("/intranet", intranetRoutes);
 
 module.exports = mainRouter;
