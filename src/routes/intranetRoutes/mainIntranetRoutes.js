fdcb10e81d7b9d49ae7e4da335be8dba36ffbef4
clemensolana <clementesolana@outlook.com>
2024-11-29 19:24:29 +0100

    feat: agrego ruta de myProfile

--- src/routes/intranetRoutes/mainIntranetRoutes.js
+++ src/routes/intranetRoutes/mainIntranetRoutes.js
@@ -5,7 +5,8 @@ const intranetRouter = express.Router();
 // Controller require
 const mainIntranetController = require("../../controllers/intranetController/mainIntranetController");
 
-// Main routes
+// Login Main routes
 intranetRouter.get("/login", mainIntranetController.login);
+intranetRouter.get("/myProfile", mainIntranetController.myProfile);
 
 module.exports = intranetRouter;
